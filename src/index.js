f5694450eda8c838d3ddc01dc2895666fa2fd753
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-04-23 22:41:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter
-        basename="/goit-test"
-      >
+      <BrowserRouter basename="/goit-test">
         <App />
       </BrowserRouter>
     </Provider>
