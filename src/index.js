1af1292f7c49f6f60f7d1a8bebda0e146944a9a6
vadrille <109307947+vadrille12@users.noreply.github.com>
2023-04-24 00:24:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="goit-test">
+      <BrowserRouter basename="goit-tweets">
         <App />
       </BrowserRouter>
     </Provider>
